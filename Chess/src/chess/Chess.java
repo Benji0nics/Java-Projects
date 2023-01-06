51363911decf319332e5ecca877c507edf0b16a1
Benji0nics <97005983+Benji0nics@users.noreply.github.com>
2023-01-06 14:04:34 -0600

    Update Chess.java

--- Chess/src/chess/Chess.java
+++ Chess/src/chess/Chess.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chess;
 import java.util.*;
 
-/**
- *
- * @author benjamin
- */
+//author Benjamin Burnham
 class Board{
     public static Object[][] board = new Object[8][8];
     Board(){
